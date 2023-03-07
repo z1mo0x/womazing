2b37d6e96732e7cf5586f2be401771f1a41c0b8a
Z1mo0x <droweckiy@mail.ru>
2023-03-07 13:57:44 +0300

    first page

--- js/burger.js
+++ js/burger.js
@@ -8,5 +8,5 @@ burger.addEventListener('click', () => {
     burger.classList.toggle('burger-active');
     list.classList.toggle('list-active');
     body.classList.toggle('scroll_off');
-    html.classList.toggle('scroll_off')
+    html2.classList.toggle('scroll_off');
 });
